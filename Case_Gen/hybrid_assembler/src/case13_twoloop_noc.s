02d23db42cf3d7697a90546089da272d52fa591a
lugy35 <1826325206@qq.com>
2024-10-27 18:48:56 +0800

    case13-done

--- Case_Gen/hybrid_assembler/src/case13_twoloop_noc.s
+++ Case_Gen/hybrid_assembler/src/case13_twoloop_noc.s
@@ -56,17 +56,14 @@ noc_req (comd_type=2, bar=0)
 noc_req (comd_type=4, bar=0)
 
 
-# 需要加@3000
-
-# dma wr
 NOC_cfg (addr=96  , wdata=0 )
 NOC_cfg (addr=97  , wdata=0 )
 NOC_cfg (addr=98  , wdata=0 ) #广播
 NOC_cfg (addr=99  , wdata=0 )
 NOC_cfg (addr=100 , wdata=0 )
-NOC_cfg (addr=101 , wdata=0 )
+NOC_cfg (addr=101 , wdata=4096 ) # bank4 4096
 NOC_cfg (addr=102 , wdata=0 )
-NOC_cfg (addr=103 , wdata=1024)
+NOC_cfg (addr=103 , wdata=520) # 从第8个数开始取
 NOC_cfg (addr=104 , wdata=0 )
 NOC_cfg (addr=105 , wdata=0 )
 NOC_cfg (addr=106 , wdata=0 )
@@ -75,20 +72,87 @@ NOC_cfg (addr=108 , wdata=1 )
 NOC_cfg (addr=109 , wdata=0 )
 NOC_cfg (addr=110 , wdata=0 )
 NOC_cfg (addr=111 , wdata=0)
-NOC_cfg (addr=112 , wdata=15)   # loop lenth
+NOC_cfg (addr=112 , wdata=14)   # loop lenth 取15个数
 NOC_cfg (addr=113 , wdata=0)
-NOC_cfg (addr=114 , wdata=15)   # ping lenth
+NOC_cfg (addr=114 , wdata=14)   # ping lenth
 NOC_cfg (addr=115 , wdata=0 )
 NOC_cfg (addr=116 , wdata=0 )   
 NOC_cfg (addr=117 , wdata=0 )   
 NOC_cfg (addr=118 , wdata=0 )
 noc_req (comd_type=3, bar=0 )
 noc_req (comd_type=4, bar=0 )
+
+# dma rd 
+NOC_cfg (addr=64 , wdata=0 )
+NOC_cfg (addr=65 , wdata=0 )
+NOC_cfg (addr=66 , wdata=0 )
+NOC_cfg (addr=67 , wdata=0 )
+NOC_cfg (addr=68 , wdata=4100)      # 从第4个数开始
+NOC_cfg (addr=69 , wdata=0 )
+NOC_cfg (addr=70 , wdata=2048 )     # 写出的目标地址
+NOC_cfg (addr=71 , wdata=0 )
+NOC_cfg (addr=72 , wdata=0 )
+NOC_cfg (addr=73 , wdata=0 )
+NOC_cfg (addr=74 , wdata=0 )
+NOC_cfg (addr=75 , wdata=1 )
+NOC_cfg (addr=76 , wdata=0 )
+NOC_cfg (addr=77 , wdata=0 )
+NOC_cfg (addr=78 , wdata=0 )
+NOC_cfg (addr=79 , wdata=7)    # 写出7个数
+NOC_cfg (addr=80 , wdata=0 )
+NOC_cfg (addr=81 , wdata=7)
+NOC_cfg (addr=82 , wdata=0 )
+NOC_cfg (addr=83 , wdata=0 )
+noc_req (comd_type=2, bar=0)
+noc_req (comd_type=4, bar=0)
 MQ_NOP(bar=0)
 MQ_NOP(bar=0)
 MQ_NOP(bar=0)
 MQ_NOP(bar=0)
 MQ_NOP(bar=0)
 MQ_NOP(bar=0)
-next_fetch_is_cpu
-wfi
+MQ_NOP(bar=0)
+MQ_NOP(bar=0)
+MQ_NOP(bar=0)
+MQ_NOP(bar=0)
+MQ_NOP(bar=0)
+MQ_NOP(bar=0)
+MQ_NOP(bar=0)
+MQ_NOP(bar=0)
+
+###################################################################################
+
+# dma wr
+# NOC_cfg (addr=96  , wdata=0 )
+# NOC_cfg (addr=97  , wdata=0 )
+# NOC_cfg (addr=98  , wdata=0 ) #广播
+# NOC_cfg (addr=99  , wdata=0 )
+# NOC_cfg (addr=100 , wdata=0 )
+# NOC_cfg (addr=101 , wdata=0 )
+# NOC_cfg (addr=102 , wdata=0 )
+# NOC_cfg (addr=103 , wdata=1024)
+# NOC_cfg (addr=104 , wdata=0 )
+# NOC_cfg (addr=105 , wdata=0 )
+# NOC_cfg (addr=106 , wdata=0 )
+# NOC_cfg (addr=107 , wdata=0 )
+# NOC_cfg (addr=108 , wdata=1 )
+# NOC_cfg (addr=109 , wdata=0 )
+# NOC_cfg (addr=110 , wdata=0 )
+# NOC_cfg (addr=111 , wdata=0)
+# NOC_cfg (addr=112 , wdata=15)   # loop lenth
+# NOC_cfg (addr=113 , wdata=0)
+# NOC_cfg (addr=114 , wdata=15)   # ping lenth
+# NOC_cfg (addr=115 , wdata=0 )
+# NOC_cfg (addr=116 , wdata=0 )   
+# NOC_cfg (addr=117 , wdata=0 )   
+# NOC_cfg (addr=118 , wdata=0 )
+# noc_req (comd_type=3, bar=0 )
+# noc_req (comd_type=4, bar=0 )
+# MQ_NOP(bar=0)
+# MQ_NOP(bar=0)
+# MQ_NOP(bar=0)
+# MQ_NOP(bar=0)
+# MQ_NOP(bar=0)
+# MQ_NOP(bar=0)
+# next_fetch_is_cpu
+# wfi
