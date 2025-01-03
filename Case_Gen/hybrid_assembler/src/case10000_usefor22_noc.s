1d636ed830d987f301a2b11f1682179cbbbf44d2
luguangyang <1826325206@qq.com>
2025-01-03 15:03:33 +0800

    992

--- Case_Gen/hybrid_assembler/src/case10000_usefor22_noc.s
+++
@@ -1,3 +0,0 @@
-
-VQ_scache_rd_en(addr=0,size=byte,sign_ext=1,rd_cycle_num=170,wait_type=1,cfifo_en=1,bar=0)
-VQ_scache_rd_en(addr=0,size=word,sign_ext=1,rd_cycle_num=525,wait_type=1,cfifo_en=1,bar=0)
---
+++ Case_Gen/hybrid_assembler/src/case10000_usefor992_noc.s
@@ -0,0 +1,3 @@
+
+VQ_scache_rd_en(addr=0,size=byte,sign_ext=1,rd_cycle_num=170,wait_type=1,cfifo_en=1,bar=0)
+VQ_scache_wr_en(addr=256,size=byte,wr_cycle_num=40,wait_type=0,cfifo_en=1,bar=0)
